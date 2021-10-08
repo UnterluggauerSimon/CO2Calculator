599fd1a29d08f532db279c6dfc8eb13497b33697
Simon Unterluggauer <s.unterluggauer@navax.com>
2021-10-08 19:59:22 +0200

    init variables

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,19 @@
+import java.util.Arrays;
+
 public class Calculator {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        int a = 1;
+        double b = 20;
+        boolean c = true;
+        char d = '-';
+        String e = "abc";
 
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
     }
 }
